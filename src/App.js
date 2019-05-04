dc5508c0730f01bb62429a496a85bfcf1ccff51c
Christopher Maier <mail@christophermaier.com.au>
2019-05-04 19:34:36 +1000

    Add, Update & Delete working. Adding, not Updating bug fixed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Amplify, {API, graphqlOperation } from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import { listNotes } from './graphql/queries';
-import { createNote } from './graphql/mutations';
+import { createNote, updateNote, deleteNote } from './graphql/mutations';
 Amplify.configure(aws_exports);
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [note, setNote] = useState("");
+  const [noteId, setNoteId] = useState ("");
+  const [noteIndex, setNoteIndex] = useState ("");
+  const [deletingId, setDeletingId] = useState ("");
 
   useEffect(() => {
     handleListNotes();
@@ -20,16 +23,70 @@ const App = () => {
     setNotes(data.listNotes.items);
   };
 
+  const hasExistingNote = () => {
+    if (noteId) {
+      const isNote = notes.findIndex(note => note.id === noteId) > -1;
+      return isNote;
+    }
+    return false;
+  }
+
+  const hasNote = () => {
+    if (note.trim()) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleUpdateNote = async () => {
+    const payload = { id: noteId, note }
+    const { data } = await API.graphql(graphqlOperation(updateNote, { input: payload }))
+    const updatedNote = data.updateNote;
+    const updatedNotes = [
+      ...notes.slice(0, noteIndex),
+      updatedNote,
+      ...notes.slice(noteIndex + 1)
+    ]
+    setNotes(updatedNotes)
+    setNote("")
+    setNoteId('')
+  }
+
+  const handleDelete = async id => {
+    setDeletingId(id)
+    const payload = { id }
+    const { data } = await API.graphql(graphqlOperation(deleteNote, { input: payload }))
+    const deletedNoteId = data.deleteNote.id;
+    const deletedNoteIndex = notes.findIndex(note => note.id === deletedNoteId)
+    const updatedNotes = [
+      ...notes.slice(0, deletedNoteIndex),
+      ...notes.slice(deletedNoteIndex + 1)
+    ]
+    setNotes(updatedNotes);
+    setDeletingId("");
+  }
+
   const handleAddNote = async event => {
     event.preventDefault()
-    const payload = { note } 
-    const { data } = await API.graphql(graphqlOperation(createNote, { input: payload })
-    );
-    const newNote = data.createNote
-    const updatedNotes = [newNote, ...notes];
-    setNotes(updatedNotes);
-    setNote("");
+
+    if (hasExistingNote()) {
+      handleUpdateNote()
+    } else if (hasNote()) {
+      const payload = { note } 
+      const { data } = await API.graphql(graphqlOperation(createNote, { input: payload })
+      );
+      const newNote = data.createNote
+      const updatedNotes = [newNote, ...notes];
+      setNotes(updatedNotes);
+      setNote("");
+    };
   }
+
+  const handleSetNote = ({ note, id }, index) => {
+    setNote(note);
+    setNoteId(id);
+    setNoteIndex(index);
+  };
 
   return (
     <div className="flex flex-column items-center justify-center bg-washed-green pa3">
@@ -43,14 +100,24 @@ const App = () => {
             className="pa2 f4"
             onChange={({ target }) => setNote(target.value)}
           />
-          <button className="pa2 f4" type="submit">Add</button>
+          <button className="pa2 f4" type="submit">
+            {noteId ? "Update" : "Add"}
+          </button>
         </form>
         { /* Note List */}
         <div>
           {notes.map((item, i) => (
             <div key={item.id} className="flex items-center">
-              <li className="list pa1 f3">{item.note}</li>
-              <button className="bg-transparent bn f4">
+              <li 
+                onClick={() => handleSetNote(item, i)}
+                className="list pa1 f3"
+                style={{ color: deletingId === item.id && 'red'}}  
+              >
+                {item.note}</li>
+              <button 
+                onClick={() => handleDelete(item.id)}  
+                className="bg-transparent bn f4"
+              >
                 <span>&times;</span>
               </button>
             </div>
