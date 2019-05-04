66f521393e6d7177c6764a789cab3a9983171a9f
Christopher Maier <mail@christophermaier.com.au>
2019-05-04 18:39:38 +1000

    createNote mutation works correctly.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,46 @@ import Amplify, {API, graphqlOperation } from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import { listNotes } from './graphql/queries';
+import { createNote } from './graphql/mutations';
 Amplify.configure(aws_exports);
 
 const App = () => {
-  const [note, setNotes] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
 
   useEffect(() => {
     handleListNotes();
   }, []);
 
-  const handleListNotes= async () => {
+  const handleListNotes = async () => {
     const { data } = await API.graphql(graphqlOperation(listNotes));
     console.log(data);
     setNotes(data.listNotes.items);
   };
 
+  const handleAddNote = async event => {
+    event.preventDefault()
+    const payload = { note } 
+    const { data } = await API.graphql(graphqlOperation(createNote, { input: payload })
+    );
+    const newNote = data.createNote
+    const updatedNotes = [newNote, ...notes];
+    setNotes(updatedNotes);
+    setNote("");
+  }
+
   return (
     <div className="flex flex-column items-center justify-center bg-washed-green pa3">
       <h1 className="code f2">Amplify Notetaker</h1> 
         { /* Note Form */}
-        <form action="" className="mb3">
-          <input type="text" placeholder="Write Your Note" className="pa2 f4"/>
+        <form onSubmit={handleAddNote} action="" className="mb3">
+          <input 
+            type="text"
+            value={note}
+            placeholder="Write Your Note"
+            className="pa2 f4"
+            onChange={({ target }) => setNote(target.value) }
+          />
           <button className="pa2 f4" type="submit">Add</button>
         </form>
         { /* Note List */}
